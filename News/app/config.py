b95371be15d8b1704051f20337057762949d127a
annngure <annwanjikungure@gmail.com>
2022-01-30 19:53:34 +0300

    add configuration

---
+++ News/app/config.py
@@ -0,0 +1,18 @@
+import os
+
+class Config:
+    '''
+    configuration parent class
+    '''
+class ProdConfig(Config):
+    '''
+    configuration child class
+
+    '''
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    DEGBUG = True
