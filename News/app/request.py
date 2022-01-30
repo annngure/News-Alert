c552a89622bc011241ad7743792ed6aa98b7892d
annngure <annwanjikungure@gmail.com>
2022-01-30 21:04:56 +0300

    add request file

--- News/app/request.py
+++ News/app/request.py
@@ -1,3 +1,12 @@
 from app import app
+import urllib.request,json
+from .models from NEWS
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting news base url
+base_url = app.config['NEWS_API_BASE_URL']
+
+def get_news(category):
+    '''
+    
