3e8ba7e918da0d5f9a1972e40d3b3d7dcf6eb0d0
annngure <annwanjikungure@gmail.com>
2022-02-01 09:22:08 +0300

    add news.html

--- ALERT/app/views.py
+++ ALERT/app/views.py
@@ -1,14 +1,19 @@
 from flask import render_template
 from app import app
+from .requests import get_news,get_news
 #views
-@app.route('/')
-def index():
+@app.route('/movie/<int:headlines>')
+def news(headlines):
     '''
     View root page fuction that returns the index page and its data
     '''
     #getting headlines
    
-    business_news=get_news('popular')
-    print(business_news)
-    title='NEWS ALERT!'
-    return render_template('index.html',title=title,business=business_news)
+    # business_news=get_news('popular')
+    # print(business_news)
+    # title='NEWS ALERT!'
+    # return render_template('index.html',title=title,business=business_news)
+    news = get_news(headlines)
+    title = f'{news.title}'
+
+    return render_template('news.html',title = title,news = news)
