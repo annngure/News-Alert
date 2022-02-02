5ee6f2df1ceeb576aa6ce37a1033a74db38332fe
annngure <annwanjikungure@gmail.com>
2022-02-02 08:42:12 +0300

    add search request

--- ALERT/app/request.py
+++ ALERT/app/request.py
@@ -1,35 +1,32 @@
 from app import app
 import urllib.request,json
-from .models import news
+from .models import news,headlines,business
 
-
-breakpoint()
 News = news.News
 
-#Getting the api key
+#the  api keys available
 api_key = app.config['NEWS_API_KEY']
+sources_url= app.config['SOURCE_BASE_API_URL']
+top_headlines_news_url=app.config['TOP_HEADLINES_BASE_API_URL']
+business_top_headlines_url=app.config['BUSINESS_TOP_HEADLINES']
+# base_url = app.config['NEWS_API_BASE_URL']
 
-#Getting the news base url
-base_url = app.config['NEWS_API_BASE_URL']
-
-def get_news(category):
+def get_all_news_sources(source):
     '''
     Function that gets json response to the url request
     '''
-    get_news_url = base_url.format(category,api_key)
+    get_news_url = sources_url.format(api_key)
 
     with urllib.request.urlopen(get_news_url) as url:
-        get_news_data =url.read()
-        get_news_response = json.loads(get_news_data)
+          sources_data =url.read()
+          sources_response = json.loads(source_data)
+          sources_result = None
+          if sources_response['results']:
+             sources_items = source_response['results']
+             sources_results = process_results(source_items)
+    return sources_results
 
-        news_result = None
-        
-        if get_news_response['results']:
-            news_result_list = get_news_response['results']
-            news_result = process_results(news_result_list)
-        return news_result
-
-def process_results(news_list):
+def process_results(source_list):
     '''
     Function that process the result and transform them into a list of objects
     
@@ -39,40 +36,124 @@ def process_results(news_list):
     returns:
         news_result : A list of news objects
     '''
-    news_results = []
-    for news_item in news_list:
-        image= news_item.get('image')
+    sources_results = []
+    for news_item in sources_list:
+        image=news_item.get('image')
         title=news_item.get('title')
         description=news_item.get('description')
         url=news_item.get('url')
         time=news_item.get('publisheAt')
         content=news_item.get('content')
-        if poster:
-            news_object =News(image,title,description,url,publisheAt,content)
-            news_result.append(news_object)
-        return news_result
-# getting to news
-def get_news(title):
-    get_news_details_url= base_url.format(title,api_key)
+        new_source =News(image,title,description,url,publisheAt,content)
+        sources_results.append(news_object)
+    
+    return sources_results
 
-    with urllib.request.urlopen(get_news_details_url) as url:
-        news_details_data = url.read()
-        news_details_response = json.loads(news_details_data)
 
-        news_object = None
-        if news_details_response:
-            image = news_details_response.get('urlToImage')
-            title = news_details_response.get('title')
-            description=news_details_response.get('description')
-            url=news_details_response.get('url')
-            time=news_details_response.get('publisheAt')
-            content=news_details_response.get('content')
-            news_object = News(image,title,description,url,publisheAt,content)
-    return news_object
+
+def get_all_news_headlines(source):
+    '''
+    function to pass top-headlines
+    '''
+    top_headlines_news_url= top_headlines_news_url.format(source,api_key)
+
+    with urllib.request.urlopen(top_headlines_news_url) as url:
+        headlines_data = url.read()
+        headlines_response = json.loads(headlines_data)
+        headlines_results = None
+        if headlines_response['results']:
+            headlines_itens = headlines_response['results']
+            headlines_results = process_all_headlines_data(headlines_list)
+    return headlines_results
+
+def process_all_headlines_data(headlines_list):
+    """
+   Converting  data given as the Top-Headlines class.
+    """
+    headlines_processed_results = []
+    for news_item in headlines_list:
+        image=news_item.get('image')
+        title=news_item.get('title')
+        description=news_item.get('description')
+        url=news_item.get('url')
+        time=news_item.get('publisheAt')
+        content=news_item.get('content')
+        new_headlines =Headlines(image,title,description,url,publisheAt,content)
+        headlines_processed_results.append(news_headlines)
+    return  headlines_processed_results
+#popular news
+def get_popular_news():
+    '''
+    function will get popular news and then to pass response to process_all_porpular
+    '''
+    popular_news_url = popular_news_url.format(api_key)
+
+    with urllib.request.urlopen(popular_news_url) as url:
+        popular_data = url.read()
+        popular_response = json.loads(popular_data)
+        popular_results = None
+
+        if popular_response['results']:
+            popular_results_list = popular_response['results']
+            popular_results = process_all_popular_results(popular_results_list)
+    return popular_results
+
+
+def process_all_popular_results(popular_results_list):
+    '''
+    responsible to convert data passed through get_popular_news() method
+     '''
+    popular_results = []
+    for news_item in popular_results_list:
+        image=news_item.get('image')
+        title=news_item.get('title')
+        description=news_item.get('description')
+        url=news_item.get('url')
+        time=news_item.get('publisheAt')
+        content=news_item.get('content')
+        new_popular =Popular(image,title,description,url,publisheAt,content)
+        popular_results.append(news_popular)
+    return  popular_results
+
+#business news
+
+def get_business_headlines():
+    '''
+    get business headlines and pass response to process_all_business_result()method
+    '''
+    business_headlines_url = business_top_headlines_url.format(api_key)
+    with urllib.request.urlopen(business_headlines_url) as url:
+        business_headlines_data = url.read()
+        business_headlines_response = json.loads(business_headlines_data)
+        business_headlines_results = None
+        if business_headlines_response['results']:
+           business_headlines_results_list = business_headlines_response['results']
+           business_headlines_results = process_all_business_result(business_headlines_results_list)
+    return business_headlines_results
+
+def process_all_business_headlines_results(business_headlines_results_list):
+    '''
+    responsible to convert data passed through get_business_headlines() method
+    '''
+    
+    business_headlines_results = []
+    for news_item in business_headlines_results_list :
+        image=news_item.get('image')
+        title=news_item.get('title')
+        description=news_item.get('description')
+        url=news_item.get('url')
+        time=news_item.get('publisheAt')
+        content=news_item.get('content')
+
+        business_headlines_object = Business(image,title,description,url,publisheAt,content)
+        business_headlines_results.append(business_headlines_object)
+        
+    return business_headlines_results
+
 
 # search news
-def search_news(news_name):
-    search_news_url='https://newsapi.org/v2/everything?q={}&from=2022-02-01&sortBy=popularity&apiKey={}'.formart(api_key,news_name)
+def search_news(source):
+    search_news_url='https://newsapi.org/v2/everything?q={}&apiKey={}'.formart(source,api_key)
     with urllib.request.urlopen(search_news_url) as url:
         search_news_data = url.read()
         search_news_response = json.loads(search_news_data)
@@ -81,6 +162,6 @@ def search_news(news_name):
 
         if search_news_response['results']:
             search_news_list = search_news_response['results']
-            search_news-results = process_results(search_news_list)
+            search_news_results = process_all_business_headlines_results(search_news_list)
 
-        return search_news_results
+    return search_news_results
--- ALERT/app/views.py
+++ ALERT/app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from app import app
-from .request import get_news,get_news,search_news
+from .request import get_all_news_sources,get_popular_news,get_business_headlines,search_news
 #views
 @app.route('/')
 def index():
@@ -9,41 +9,44 @@ def index():
     '''
 #getting headlines
    
-    popular_news=get_news('popular')
+    all_news_sources =get_all_news_sources()
+    popular_news=get_popular_news()
     # print(popular_news)
-    upcoming_news=get_news('upcoming')
-    now_showing_news =get_news('now_showing')
+    # upcoming_news=get_upcoming_news()
+    business_headlines = get_business_headlines()
+
+  #  now_showing_news =get_news('now_showing')
 
     title='NEWS ALERT!'
 
     search_news = request.args.get('news_query')
     
     if search_news:
-        return redirect(url_for('search',news_name=search_news))
+        return redirect(url_for('.search',source_name=search_news))
     else:
 
-    return render_template('index.html',title=title,popular=popular_news,upcoming=upcoming_news,now_showing=now_showing_news)
+        return render_template('index.html',sources= all_news_sources,title=title,popular=popular_news,upcoming=upcoming_news,business_headlines=business_headlines)
 
 
-@app.route('/news/<int:title>')
-def news(title):
+@app.route('/source/<int:title>')
+def news_headlines(title):
     '''
     View root page fuction that returns the index page and its data
     '''
     
-    news = get_news(title)
+    news = get_all_news_headlines(title)
     title = f'{news.title}'
 
-    return render_template('news.html',title = title,news = news)
+    return render_template('news.html',title = title,headlines = news_headlines)
 
-@app.route('/search/<news_name>')
-def search(news_name):
+@app.route('/search/<source_name>')
+def search(source_name):
     '''
     view function to dispaly the search results
 
     '''
     news_name_list = news_name.split('')
     news_name_format = "+".join(news_name_list)
-    searched_news = search_news(news_name_format)
-    title =f'search result for {news_name_format}'
-    return render_template('search.html',news = searched_news)
+    searched_sources = search_news(news_name_format)
+    title =f'search result for {news_name}'
+    return render_template('search.html',results = searched_sources)
